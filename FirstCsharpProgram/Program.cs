1a5f88ef3519c12198f63e5e2d4dbeb0effce31c
Bircan E. <bircan.emtekin@hotmail.com>
2024-08-20 00:50:47 +0200

    Add project files.

---
+++ FirstCsharpProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstCsharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to your first C# program!");
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+
+        }
+    }
+}
